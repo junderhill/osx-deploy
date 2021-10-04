536443d445f69081115c13a2ecb62d640c0d8d40
Jason Underhill <junderhill@serif.com>
2021-10-04 09:47:16 +0100

    Remove a few things I don't use

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@
 source ~/.bashrc
 
 brew install tmux
-
 brew install vim
 
 brew cask install firefox
@@ -15,21 +14,13 @@ brew cask install spotify
 
 brew cask install veracrypt
 
-brew cask install nextcloud
-
-brew cask install spectacle
-
 brew cask install alfred
-
-brew install keepassxc
 
 brew cask install visual-studio-code
 
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 brew install git
-
-brew install fish
 
 mkdir ~/Code
 mkdir ~/Packages
@@ -38,9 +29,4 @@ cd ~/Packages
 git clone https://github.com/junderhill/VimConfig.git
 
 /bin/bash VimConfig/configureVim.sh
-
-sudo echo /usr/local/bin/fish >> /etc/shells
-chsh -s /usr/local/bin/fish
-
-
 
